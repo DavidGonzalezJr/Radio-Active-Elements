806d665c30ad6cdeacb847077b26e9e830ef39dd
David Gonzalez Jr <dgonzalez1@fullsail.edu>
2018-02-02 13:58:16 -0500

    Initial Commit

---
+++ GonzalezDavid_CE2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GonzalezDavid_CE2
+//
+//  Created by David Gonzalez Jr on 2/2/18.
+//  Copyright © 2018 David Gonzalez Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
